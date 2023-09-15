e6bd2dc6a57a9121c833ac21c5b2fbe80acb6c93
sport <sport@LAPTOP-61I2ACB0>
2023-09-15 21:48:50 +0300

    Add project files.

---
+++ Arrays2/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] n = { 2, 5, 7 };
+
+Console.WriteLine("The array stored is: ");
+foreach (var item in n)
+{
+    Console.Write(item.ToString() + " ");
+}
+
+Console.WriteLine();
+Console.WriteLine("The array in reverse is: ");
+
+Array.Reverse(n);
+Console.WriteLine(String.Join(" ", n));
